9833a81dfb36f61824519227014cf23d62b9f342
gfrecon <gauthierfrecon@hotmail.fr>
2019-02-14 00:17:18 +0100

    segmentation f1 pr images de ref et modif' v3

--- segmentation_fibres_v3.py
+++ segmentation_fibres_v3.py
@@ -12,35 +12,56 @@ script à appliquer à partir des zones des fibres inversées et égalisées:
 -OU logique
 -pas besoin de faire d'étiquettage
 
+
+
+
 """
 
+import skimage 
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import pydicom
+from pydicom.data import get_testdata_files
+from scipy import ndimage
+import math
+from scipy import signal
+import skimage.io as io
+from skimage.transform import rotate
+from skimage.exposure import equalize_adapthist
+
+#ouverture de l'image
+IMDIR=r"D:\Documents\Projet Mammographie\datasim-prj-phantoms-planmed-dataset-201812061411\datasim-prj-phantoms-dataset-201812061411\digital\2.16.840.1.113669.632.20.20130917.192726317.17.381"
+nom_im1="2.16.840.1.113669.632.20.20130917.192819263.200064.384.dcm"
+IMDIR2=r"D:\Documents\Projet Mammographie\datasim-prj-phantoms-planmed-dataset-201812061411\datasim-prj-phantoms-dataset-201812061411\digital\2.16.840.1.113669.632.20.20120425.94758942.2456.10"
+nom_im2="2.16.840.1.113669.632.20.20120425.94850094.200083.40.dcm"
+
+IMDIR_fuji="D:\Documents\Projet Mammographie\datasim-prj-phantoms-fujifilm-dataset-201812130858\datasim-prj-phantoms-fuji-dataset-201812130858"
+IMDIR_planmed="D:\Documents\Projet Mammographie\datasim-prj-phantoms-planmed-dataset-201812061411\datasim-prj-phantoms-dataset-201812061411\digital"
+IMDIR_hologic=r"D:\Documents\Projet Mammographie\datasim-prj-phantoms-hologic-20190125-mg-proc"
+IMDIR_ge=r"D:\Documents\Projet Mammographie\datasim-prj-phantoms-ge-20190125-mg-proc"
+
+nom_im1_ge="ge-0001-0000-00000000.dcm"
+nom_im1_hologic="hologic-MG02.dcm"
+
+
+chemin=IMDIR + "/" + nom_im1
+ds = pydicom.dcmread(chemin)
+im=ds.pixel_array
 
 plt.close('all')
 
-def correlation_mask_I(im, Lx, Ly, seuil, angle=45):
-    ''' correlation de l'image avec un mask en I de taille 2*Lx   * 2*Ly puis seuillage à seuil'''
-    
-    mask=np.zeros_like(fibre_F1_inverted)
-    mask[122-Lx:122+Lx,118-Ly:118+Ly]=np.ones([2*Lx,2*Ly])
-    mask=rotate(mask, angle)
-    mask[0,0]=np.max(mask)*np.ones([1,1])
-#    plt.imshow(mask, cmap='gray')
-#    plt.title("mask")
-#    plt.show()
-    corr_mask=signal.correlate(im, mask, mode='same')
-    max_corr_mask=np.max(corr_mask)
-    min_corr_mask=np.min(corr_mask)
-#    y, x = np.histogram(corr_mask, bins=np.arange(min_corr_mask,max_corr_mask))
-#    fig, ax = plt.subplots()
-#    plt.plot(x[:-1], y)
-#    plt.show()
-    im_corr=corr_mask>seuil
-#    plt.imshow(im_corr, cmap='gray')
-#    plt.show()
-    return(im_corr)
-    
 
 
+#redim
+    
+im_red=redim_im(im)
+[n,p]=np.shape(im_red)
+
+#isolement fibre f1
+ 
+fibre_F1=isoler(im_red, [0.11,0.22], [0.70,0.87])           
+    
 # traitement fibre F1
 
 fftc_highpass=highpass_filter(fibre_F1_inverted,Dc=5)
@@ -52,11 +73,7 @@ plt.show()
 
 im_highpass=invfft_highpass 
 
-#im_corr_I1=correlation_mask_I(fibre_F1_inverted,4,40, seuil=371, angle=45) 
-#im_corr_I2=correlation_mask_I(fibre_F1_inverted,5,60, seuil=657, angle=135) 
-#im_segmentation= (im_corr_I1+im_corr_I2)
-#plt.imshow(im_segmentation, cmap='gray')
-#plt.show()
+
 
 im_corr_I1=correlation_mask_I(im_highpass,4,40, seuil=31, angle=45) 
 im_corr_I2=correlation_mask_I(im_highpass,5,40, seuil=26, angle=135) #4,20, seuil=193, angle=135)
@@ -94,4 +111,29 @@ plt.show()
 
 im_highpass=invfft_highpass /10000
 im_corr_I1=correlation_mask_I(im_highpass,3,40, seuil=5, angle=45) 
+
+# zones carrés noir et blanc
+
+m=n//2
+x_bas=525
+x_haut=575
+y_matblanc_bas=m-30
+y_matblanc_haut=m+30
+y_matnoir_bas=m+125-30
+y_matnoir_haut=m+125+30
+
+
+
+moyenne_matblanc=np.mean(im_red[y_matblanc_bas: y_matblanc_haut, x_bas:x_haut])
+moyenne_matnoir=np.mean(im_red[y_matnoir_bas: y_matnoir_haut, x_bas:x_haut])              
+
+
+plt.imshow(im_red, cmap='gray')
+plt.plot([0,p], [m, m])
+plt.plot([x_bas, x_bas,x_haut, x_haut, x_bas], [y_matblanc_bas, y_matblanc_haut, y_matblanc_haut, y_matblanc_bas,y_matblanc_bas])
+plt.plot([x_bas, x_bas,x_haut, x_haut, x_bas], [y_matnoir_bas, y_matnoir_haut, y_matnoir_haut, y_matnoir_bas, y_matnoir_bas])
+plt.show()
+
+
+
 
