4a047d2811960eadd71a48aefd697ff4e214c991
Sébastien Villa <villa.sebastien97@gmail.com>
2019-03-28 22:14:25 +0100

    Code de détection des masses

--- Code.py
+++ Code.py
@@ -489,7 +489,7 @@ m1=io.imread(DIRMASK+"PM_mask.png")
 #masses1=isoler(im1,[0.37,0.46],[0.05,0.29])    
 [masses1,mask1]=redim_im_bis(im1.pixel_array,m1)
 [masses1,mask1]=isoler(masses1,mask1,[0.603,0.722],[0.436,0.9])
-mask1=mask1[:,:,3]
+mask1=normalisation(mask1[:,:,3])
 masses1=normalisation(masses1)
 
 im2=pydicom.dcmread(DIR+"hologic-MG02.dcm")
@@ -498,7 +498,7 @@ m2=io.imread(DIRMASK+"Hologic_mask.png")
 #masses2=isoler(im2,[0.355,0.445],[0.05,0.29])
 [masses2,mask2]=redim_im_bis(im2.pixel_array,m2)
 [masses2,mask2]=isoler(masses2,mask2,[0.603,0.722],[0.436,0.9])
-mask2=mask2[:,:,3]
+mask2=normalisation(mask2[:,:,3])
 masses2=normalisation(masses2)
 masses2=1-masses2
 
@@ -508,7 +508,7 @@ m3=io.imread(DIRMASK+"GE_mask.png")
 #masses3=isoler(im3,[0.32,0.4021],[0.05,0.29])
 [masses3,mask3]=redim_im_bis(im3.pixel_array,m3)
 [masses3,mask3]=isoler(masses3,mask3,[0.603,0.722],[0.436,0.9])
-mask3=mask3[:,:,3]
+mask3=normalisation(mask3[:,:,3])
 masses3=normalisation(masses3)
 masses3=1-masses3
 
@@ -517,20 +517,22 @@ m4=io.imread(DIRMASK+"FUJI_mask.png")
 #im4=rotation(im4.pixel_array)
 #masses4=isoler(im4,[0.35,0.44],[0.05,0.29])
 [masses4,mask4]=redim_im_bis(im4.pixel_array,m4)
-[masses4,mask4]=isoler(masses4,mask4,[0.603,0.722],[0.436,0.9])
-mask4=mask4[:,:,3]
+[masses4,mask4]=isoler(masses4,mask4,[0.603,0.722],[0.436,0.9])#0.605
+mask4=normalisation(mask4[:,:,3])
 masses4=normalisation(masses4)
 
 
 """
+
+
 fond1=ndimage.convolve(masses1,meanKernel(101))
-np.save("fond1.npy",fond1)
+np.save("fond_PM.npy",fond1)
 fond2=ndimage.convolve(masses2,meanKernel(101))
-np.save("fond2.npy",fond2)
+np.save("fond_Holo.npy",fond2)
 fond3=ndimage.convolve(masses3,meanKernel(101))
-np.save("fond3.npy",fond3)
+np.save("fond_GE.npy",fond3)
 fond4=ndimage.convolve(masses4,meanKernel(101))
-np.save("fond4.npy",fond4)
+np.save("fond_fuji.npy",fond4)
 
 #np.load("random-matrix.npy")
 
@@ -541,100 +543,42 @@ Fond2=normalisation(skimage.color.rgb2gray(io.imread("./Fond_2.png")))
 Fond3=normalisation(skimage.color.rgb2gray(io.imread("./Fond_3.png")))
 Fond4=normalisation(skimage.color.rgb2gray(io.imread("./Fond_4.png")))
 """
+#Fond1=np.load("fond_PM.npy")
+#Fond2=np.load("fond_Holo.npy")
+#
+#Fond3=np.load("fond_GE.npy")
+#
+#Fond4=np.load("fond_fuji.npy") #Ces  fonds la ne servent plus
 Fond1=np.load("fond1.npy")
 Fond2=np.load("fond2.npy")
 Fond2=1-Fond2
 Fond3=np.load("fond3.npy")
 Fond3=1-Fond3
-Fond4=np.load("fond4.npy")
+Fond4=np.load("fond4.npy") #Ces fonds la marchent
+
+K4=101 #Le plus que je peux mettre avant une erreur mémoire, mais ca marche
+#Donc je le fixe comme réference
+K3=round(K4*(1/im3.ImagerPixelSpacing[0]*im4.ImagerPixelSpacing[0]))
+K2=round(K4*(1/im2.ImagerPixelSpacing[0]*im4.ImagerPixelSpacing[0]))
+K1=round(K4*(1/im1.ImagerPixelSpacing[0]*im4.ImagerPixelSpacing[0]))
+"""
+fond1=ndimage.convolve(masses1,meanKernel(K1))
+np.save("fond1_adapt.npy",fond1)
+fond2=ndimage.convolve(masses2,meanKernel(K2))
+np.save("fond2_adapt.npy",fond2)
+fond3=ndimage.convolve(masses3,meanKernel(K3))
+np.save("fond3_adapt.npy",fond3)
+fond4=ndimage.convolve(masses4,meanKernel(K4))
+np.save("fond4_adapt.npy",fond4)
+
+Fond1=np.load("fond1_adapt.npy")
+Fond2=np.load("fond2_adapt.npy")
+
+Fond3=np.load("fond3_adapt.npy")
+Fond3=1-Fond3
+Fond4=np.load("fond4_adapt.npy")
+""" #♦Ces fonds marchent pas torp
 #utiliser numpy save, ou sauvegarder en noir et blanc
-
-def test1():
-
-    plt.figure(1)
-    plt.subplot(4,2,1)
-    plt.imshow(masses1)
-    plt.title("PlanMed")
-    plt.subplot(4,2,2)
-    plt.imshow(pipeline1(masses1))
-    print("L'image de Medplan a une moyenne de {}, \n un écart-type de {},\n un contraste de {},"
-          "\n un rapport ecart-type / moyenne de {},\n une skewness de {}, \n et un kurtosis de {}".format(np.mean(masses1),
-          np.std(masses1-np.mean(masses1)),1-np.min(masses1),np.std(masses1)/np.mean(masses1),
-          scipy.stats.skew(masses1, axis=None),scipy.stats.kurtosis(masses1, axis=None)))
-    plt.subplot(4,2,3)
-    plt.imshow(masses2)
-    plt.title("Hologic")
-    plt.subplot(4,2,4)
-    plt.imshow(pipeline1(masses2))
-    print("L'image de Hologic a une moyenne de {}\n, un écart-type de {}\n, un contraste de {}"
-          ",\n un rapport ecart-type / moyenne de {},\n une skewness de {}, \n et un kurtosis de {}"
-          .format(np.mean(masses2),
-          np.std(masses2-np.mean(masses2)),1-np.min(masses2),np.std(masses2)/np.mean(masses2),
-          scipy.stats.skew(masses2, axis=None),scipy.stats.kurtosis(masses2, axis=None)))
-    plt.subplot(4,2,5)
-    plt.imshow(masses3)
-    plt.title("GE")
-    plt.subplot(4,2,6)
-    plt.imshow(pipeline1(masses3))
-    print("L'image de GE a une moyenne de {}\n, un écart-type de {}\n, un contraste de {},"
-          "\n un rapport ecart-type / moyenne de {},\n une skewness de {}, \n et un kurtosis de {}"
-          .format(np.mean(masses3),
-          np.std(masses3-np.mean(masses3)),1-np.min(masses3),np.std(masses3)/np.mean(masses3),
-          scipy.stats.skew(masses3, axis=None),scipy.stats.kurtosis(masses3, axis=None)))
-    plt.subplot(4,2,7)
-    plt.imshow(masses4)
-    plt.title("Fuji")
-    plt.subplot(4,2,8)
-    plt.imshow(pipeline1(masses4))
-    print("L'image de Medplan a une moyenne de {}\n, un écart-type de {}\n, un contraste de {},"
-          "\n un rapport ecart-type / moyenne de {},\n une skewness de {}, \n et un kurtosis de {}"
-          .format(np.mean(masses4),
-          np.std(masses4-np.mean(masses4)),1-np.min(masses4),np.std(masses4)/np.mean(masses4),
-          scipy.stats.skew(masses4, axis=None),scipy.stats.kurtosis(masses4, axis=None)))
-    
-    plt.figure(2)
-    plt.subplot(4,2,1)
-    plt.imshow(masses1)
-    plt.title("PlanMed")
-    plt.subplot(4,2,2)
-    plt.imshow(pipeline2(masses1))
-    plt.subplot(4,2,3)
-    plt.imshow(masses2)
-    plt.title("Hologic")
-    plt.subplot(4,2,4)
-    plt.imshow(pipeline2(masses2))
-    plt.subplot(4,2,5)
-    plt.imshow(masses3)
-    plt.title("GE")
-    plt.subplot(4,2,6)
-    plt.imshow(pipeline2(masses3))
-    plt.subplot(4,2,7)
-    plt.imshow(masses4)
-    plt.title("Fuji")
-    plt.subplot(4,2,8)
-    plt.imshow(pipeline2(masses4))
-    
-    plt.figure(3)
-    plt.subplot(4,2,1)
-    plt.imshow(masses1)
-    plt.title("PlanMed")
-    plt.subplot(4,2,2)
-    plt.imshow(pipeline3(masses1))
-    plt.subplot(4,2,3)
-    plt.imshow(masses2)
-    plt.title("Hologic")
-    plt.subplot(4,2,4)
-    plt.imshow(pipeline3(masses2))
-    plt.subplot(4,2,5)
-    plt.imshow(masses3)
-    plt.title("GE")
-    plt.subplot(4,2,6)
-    plt.imshow(pipeline3(masses3))
-    plt.subplot(4,2,7)
-    plt.imshow(masses4)
-    plt.title("Fuji")
-    plt.subplot(4,2,8)
-    plt.imshow(pipeline3(masses4))
 
 
 #hp=skimage.filters.laplace(masses,10)
@@ -681,8 +625,9 @@ plt.imshow(C5)
 Essayer d'inverser les images Hologic et GE pour voir si cela change quelque chose  ET LE FOND!!!
 essayer de floutter proportionnelement à la metrique
 Idem pour extraire le fond.
+Faire la normalisation de facon automatique en testant la moyenne de l'image normalisée
 """
-def pipeline_corr(im,block_size = 8,sig=8,disk_size=5):
+def pipeline_corr(im,block_size = 8,disk_size=5):
     #im=ndimage.convolve(masses,gradlap()[2])
     #im_contloc=skimage.exposure.equalize_adapthist(im)
     #im=ndimage.convolve(im_contloc,gaussianKernel(block_size,sig))
@@ -699,9 +644,10 @@ def pipeline_corr(im,block_size = 8,sig=8,disk_size=5):
     im_nl = denoise_nl_means(im, h=1.15 * sigma_est, fast_mode=False,
                            **patch_kw)
     im_med=scipy.signal.medfilt(im_nl,7)
-    im=skimage.filters.gaussian(im_med)
+    #im=skimage.filters.gaussian(im_med)
+    im=ndimage.convolve(im_med,gaussianKernel(block_size,max(block_size//4,1)))
     im=scipy.signal.medfilt(im,7)
-    im_corr=scipy.signal.correlate(im,skimage.morphology.selem.disk(disk_size),mode='same')
+    im_corr=scipy.signal.correlate(im,skimage.morphology.selem.disk(disk_size))#,mode='same')
     #skimage.filters.try_all_threshold(im_corr)
     t=skimage.filters.threshold_minimum(im_corr)
     imt=(im_corr>t).astype(int)
@@ -723,9 +669,9 @@ def test_corr(dep,fin,pas):
         plt.subplot(4,(fin-dep)//pas,(i+1))
         plt.imshow(pipeline_corr(masses1-Fond1,disk_size=dep+i*pas))
         plt.subplot(4,(fin-dep)//pas,(fin-dep)//pas+(i+1))
-        plt.imshow(pipeline_corr(masses2-(1-Fond2),disk_size=dep+i*pas))
+        plt.imshow(pipeline_corr(masses2-Fond2,disk_size=dep+i*pas))
         plt.subplot(4,(fin-dep)//pas,2*(fin-dep)//pas+(i+1))
-        plt.imshow(pipeline_corr(masses3-(1-Fond3),disk_size=dep+i*pas))
+        plt.imshow(pipeline_corr(masses3-Fond3,disk_size=dep+i*pas))
         plt.subplot(4,(fin-dep)//pas,3*(fin-dep)//pas+(i+1))
         plt.imshow(pipeline_corr(masses4-Fond4,disk_size=dep+i*pas))
     plt.show()
@@ -767,24 +713,66 @@ def pipeline_finale(taille_masse=0.75):
     #du rapport des pixels, parceque l'on considère des volumes
     D1=round(D4*(1/im1.ImagerPixelSpacing[0]*im4.ImagerPixelSpacing[0])**2)
     D2=round(D4*(1/im2.ImagerPixelSpacing[0]*im4.ImagerPixelSpacing[0])**2)
-    D3=round(D4*(1/im3.ImagerPixelSpacing[0]*im4.ImagerPixelSpacing[0])**2)
-    res1=pipeline_corr(masses1-Fond1,disk_size=D1)
-    res2=pipeline_corr(masses2-Fond2,disk_size=D2)
-    res3=pipeline_corr(masses3-Fond3,disk_size=D3)
-    res4=pipeline_corr(masses4-Fond4,disk_size=D4)
-    [l1,n1]=scipy.ndimage.measurements.label(res1)
-    [l2,n2]=scipy.ndimage.measurements.label(res2)
+    D3=round(D4*(1/im3.ImagerPixelSpacing[0]*im4.ImagerPixelSpacing[0])**2)#Ca marche mieux au carré, 
+    #mais je sais pas pourquoi
+    res1=pipeline_corr(masses1-Fond1,block_size=D1//2,disk_size=D1)#Application de la pipeline
+    res2=pipeline_corr(masses2-Fond2,block_size=D2//2,disk_size=D2)
+    res3=pipeline_corr(masses3-Fond3,block_size=D3//2,disk_size=D3)
+    res4=pipeline_corr(masses4-Fond4,block_size=D4//2,disk_size=D4)
+    res1=res1[D1:-D1,D1:-D1]
+    res2=res2[D2:-D2,D2:-D2]
+    res3=res3[D3:-D3,D3:-D3]#Redimensionnement pour enlever l'effet de la corrélation sur la taille
+    res4=res4[D4:-D4,D4:-D4]#et aisni pouvoir comparer au mask
+    [l1,n1]=scipy.ndimage.measurements.label(res1) #Fonction pour compter le nombre de masses détectées
+    [l2,n2]=scipy.ndimage.measurements.label(res2)#Apporte un autre point de vue du score
     [l3,n3]=scipy.ndimage.measurements.label(res3)
     [l4,n4]=scipy.ndimage.measurements.label(res4)
-    plt.subplot(4,1,1)
-    plt.imshow(l1)
+    M1=ajoutermask(res1,mask1) #Pour pouvoir afficher les résultats sur le masque
+    M2=ajoutermask(res2,mask2)
+    M3=ajoutermask(res3,mask3)
+    M4=ajoutermask(res4,mask4)
+    resultats_masses(res1,mask1) #Affichage des résultats chiffrés
+    resultats_masses(res2,mask2)
+    resultats_masses(res3,mask3)
+    resultats_masses(res4,mask4)
+    plt.subplot(4,1,1) #Il reste à afficher les masses et les mask sur les mêmes images
+    plt.imshow(M1)
     plt.subplot(4,1,2)
-    plt.imshow(l2)
+    plt.imshow(M2)
     plt.subplot(4,1,3)
-    plt.imshow(l3)
+    plt.imshow(M3)
     plt.subplot(4,1,4)
-    plt.imshow(l4)
-    return([l1,n1],[l2,n2],[l3,n3],[l4,n4])
+    plt.imshow(M4)
+    return(l1,l2,l3,l4)
     
-#Il faut faire 1-res2/3
+def resultats_masses(im,mask):
+    [im1,m1]=isoler(im,mask,[0.5,1],[0.738,1])
+    [im2,m2]=isoler(im,mask,[0.5,1],[0.424,0.738])
+    [im3,m3]=isoler(im,mask,[0.5,1],[0.157,0.424])
+    [im4,m4]=isoler(im,mask,[0,1],[0,0.157])
+    [im5,m5]=isoler(im,mask,[0,0.5],[0.738,1])
+    [im6,m6]=isoler(im,mask,[0,0.5],[0.424,0.738])
+    [im7,m7]=isoler(im,mask,[0,0.5],[0.157,0.424])
+    print ('Les résultats globaux sont :')
+    resultat(im,mask)
+    print("Les résultats de la masse n°1 sont :")
+    resultat(im1,m1)
+    print("Les résultats de la masse n°2 sont :")
+    resultat(im2,m2)
+    print("Les résultats de la masse n°3 sont :")
+    resultat(im3,m3)
+    print("Les résultats de la masse n°4 sont :")
+    resultat(im4,m4)
+    print("Les résultats de la masse n°5 sont :")
+    resultat(im5,m5)
+    print("Les résultats de la masse n°6 sont :")
+    resultat(im6,m6)
+    print("Les résultats de la masse n°7 sont :")
+    resultat(im7,m7)
     
+def ajoutermask(im,mask): #Pour la visualisation des deux en superposé
+    [n,p]=np.shape(im)
+    M=np.zeros([n,p,3])
+    M[:,:,0]=im
+    M[:,:,2]=mask
+    return(M)
